5c7fec6951a83df1348cf545c8bffefe17f8caca
Pedro Hyvo <pedrohyvocarvalho@gmail.com>
2021-12-10 09:59:02 -0500

    Delete cypress default examples and add login using Cypress Studio

---
+++ cypress-studio/cypress/integration/login/login.spec.js
@@ -0,0 +1,16 @@
+describe('Cypress Studio code example', () => {
+  
+  it('can login successfully', () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://www.saucedemo.com/');
+    cy.get('[data-test="username"]').clear();
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').clear();
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+    cy.get('.title').should('have.text', 'Products');
+    cy.get('.title').should('be.visible');
+    /* ==== End Cypress Studio ==== */
+  });
+
+})
